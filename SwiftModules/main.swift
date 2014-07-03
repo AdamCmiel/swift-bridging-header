c1ce542f8e4be70ae0b290d22bc58125fa87ab85
Adam Cmiel <adam@famo.us>
2014-07-02 23:27:57 -0700

    Initial Commit

---
+++ SwiftModules/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftModules
+//
+//  Created by Adam Cmiel on 7/2/14.
+//  Copyright (c) 2014 adamcmiel. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
